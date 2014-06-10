b1515ece7da226b65085b4777958ad9211e93dd4
JeffreyTerry <jterry94@gmail.com>
2014-06-10 13:27:20 -0500

    more mobile friendly

--- assets/js/home/home.js
+++ assets/js/home/home.js
@@ -8,17 +8,33 @@ function goto(url){
   window.location = url;
 }
 
+function reformatElements(){
+  if($(window).width() >= 1020){
+    $('.blank-content').css('width', '180px');
+    $('.pa-container').css('padding-left', '0');
+    $('.pa-container').css('padding-right', '0');
+  }else{
+    $('.blank-content').css('width', '0');
+    $('.pa-container').css('padding-left', '90px');
+    $('.pa-container').css('padding-right', '90px');
+  }
+}
+
 $(document).ready(function(){
-    if($(window).width() >= 1020){
-        $.stellar({
-          horizontalScrolling: false
-        });
-        $('.navigation-panel').addClass('animated');
-        $('.navigation-panel').addClass('bounceInRight');
-    }else{
-        $('.navigation-panel').addClass('animated');
-        $('.navigation-panel').addClass('fadeInDown');
-    }
+  if($(window).width() >= 1020){
+    $.stellar({
+      horizontalScrolling: false
+    });
+  }else{
+    $('.blank-content').css('width', '0');
+    $('.pa-container').css('padding-left', '90px');
+    $('.pa-container').css('padding-right', '90px');
+  }
+
+
+  $(window).resize(function(){
+    reformatElements();
+  })
 });
 
 
