7b76408e51fa88ae596f7e5093c617163322a1f6
JeffreyTerry <jterry94@gmail.com>
2015-08-20 02:13:07 -0500

    cleaned up design; rewrote intro paragraph; and added new projects section and past work section

--- assets/js/home/home.js
+++ assets/js/home/home.js
@@ -1,3 +1,43 @@
 $(function () {
+  // Initial set up //
   $('[data-toggle="popover"]').popover()
+  setProjectsBoxStyle();
+
+  // Register listeners //
+  var $window = $(window);
+  $window.resize(function() {
+    setProjectsBoxStyle();
+  });
+
+  var originalCircleColor = $('.projects-box .circle').css('background-color');
+  var circleAnimationTime = 240;
+  $('.projects-box .circle').hover(function() {
+    $(this).animate({
+        'background-color': 'rgba(0, 0, 0, 0)',
+        'color': originalCircleColor,
+        'border-color': originalCircleColor
+    }, circleAnimationTime);
+  }, function() {
+    $(this).animate({
+        'background-color': originalCircleColor,
+        'color': 'black',
+        'border-color': 'rgba(0, 0, 0, 0)'
+    }, circleAnimationTime);
+  });
+
+  // Helper functions //
+  var projectsVideoHWRatio = 0.562;
+  function setProjectsBoxStyle() {
+    // Dimensions of the projects box
+    var width = $(window).width();
+    var height = width * 0.562;
+    console.log(width, height);
+
+    // The styled minimum width of the video is 400px,
+    // but for some reason it mins-out at 480px
+    // height = Math.max(height, 480);
+
+    // Set some style variables
+    $('.projects-box').css('top', '-' + height + 'px');
+  }
 });
